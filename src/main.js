49c5c8ac4d4091f3c08144a82e0a2a4f950c3be7
Simon Renhult <simon.renhult@hotmail.se>
2025-10-13 14:49:55 +0200

    Dev: Sanitize HTML and verify URLS for all feeds

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@
 import fs from "node:fs/promises";
 
 import { XMLParser, XMLBuilder, XMLValidator} from "fast-xml-parser";
+import sanitizeHtml from "sanitize-html";
 
 // NOTE(simon): Parse settings
 const rawConfig = await fs.readFile("config.json", { encoding: "utf8", }).catch(() => "[]");
@@ -92,15 +93,8 @@ const updateFeeds = async () => {
         },
     });
     const newFeeds = rawFeeds
-        .filter(({ url, text, }) => text.length !== 0)
+        .filter(({ url, text, }) => text && text.length !== 0)
         .map(({ url, text, }) => {
-            let xmlFeed = {};
-            try {
-                xmlFeed = xmlParser.parse(text, true);
-            } catch (error) {
-                console.error(`Failed to parse XML of '${url}': ${error.message}`);
-            }
-
             // NOTE(simon): Fill out default values.
             const feed = {
                 title:       undefined,
@@ -110,133 +104,140 @@ const updateFeeds = async () => {
                 articles:    [],
                 updated:     new Date(),
             };
-            if (xmlFeed.rss) {
-                const rss = xmlFeed.rss.channel;
 
-                if (rss.title) {
-                    feed.title = rss.title["#text"];
-                }
+            try {
+                let xmlFeed = xmlParser.parse(text, true);
 
-                if (rss.description) {
-                    feed.description = rss.description["#text"];
-                }
+                if (xmlFeed.rss) {
+                    const rss = xmlFeed.rss.channel;
 
-                if (rss.link && rss.link["#text"]) {
-                    feed.link = feed.id = rss.link["#text"];
-                } else {
-                    feed.link = feed.id = url;
-                }
-
-                feed.articles = rss.item.map(item => {
-                    // NOTE(simon): Fill out default values.
-                    const article = {
-                        title:      undefined,
-                        link:       undefined,
-                        id:         undefined,
-                        published:  new Date(),
-                        updated:    new Date(),
-                    };
-
-                    // NOTE(simon): Must be defined.
-                    if (item.title) {
-                        article.title = item.title["#text"];
+                    if (rss.title) {
+                        feed.title = sanitizeHtml(rss.title["#text"] ?? "");
                     }
 
-                    // NOTE(simon): The link must exist, but in case it
-                    // doesn't, use the guid instead.
-                    if (item.link) {
-                        article.link = item.link["#text"];
-                    } else if (item.guid) {
-                        article.link = item.guid["#text"];
+                    if (rss.description) {
+                        feed.description = sanitizeHtml(rss.description["#text"] ?? "");
                     }
 
-                    // NOTE(simon): The guid is optional, so use the link if it
-                    // doesn't exist.
-                    if (item.guid) {
-                        article.id = item.guid["#text"];
-                    } else if (item.link) {
-                        article.id = item.link["#text"];
+                    if (rss.link && rss.link["#text"]) {
+                        feed.link = feed.id = (new URL(rss.link["#text"])).href;
+                    } else {
+                        feed.link = feed.id = url;
                     }
 
-                    if (item.pubDate && item.pubDate["#text"]) {
-                        article.published = article.updated = new Date(item.pubDate["#text"]);
-                    }
+                    feed.articles = rss.item.map(item => {
+                        // NOTE(simon): Fill out default values.
+                        const article = {
+                            title:      undefined,
+                            link:       undefined,
+                            id:         undefined,
+                            published:  new Date(),
+                            updated:    new Date(),
+                        };
 
-                    return article;
-                });
-
-                if (rss.lastBuildDate) {
-                    feed.updated = rss.lastBuildDate["#text"];
-                } else if (rss.pubDate) {
-                    feed.updated = rss.pubDate["#text"];
-                }
-            } else if (xmlFeed.feed) {
-                const atom = xmlFeed.feed;
-
-                if (atom.title) {
-                    feed.title = atom.title["#text"];
-                }
-
-                // NOTE(simon): At most one
-                if (atom.subtitle) {
-                    feed.description = atom.subtitle["#text"] ?? "";
-                }
-
-                // NOTE(simon): Should
-                if (atom.link) {
-                    feed.link = atom.link.find(link => link["@_rel"] === "self")["@_href"] ?? url;
-                } else {
-                    feed.link = url;
-                }
-
-                // NOTE(simon): Must
-                if (atom.id) {
-                    feed.id = atom.id["#text"];
-                }
-
-                feed.articles = atom.entry.map(entry => {
-                    // NOTE(simon): Fill out default values.
-                    const article = {
-                        title:     undefined,
-                        link:      undefined,
-                        id:        undefined,
-                        published: new Date(),
-                        updated:   new Date(),
-                    };
-
-                    if (entry.title) {
-                        article.title = entry.title["#text"];
-                    }
-
-                    // NOTE(simon): Grab the link. MUST
-                    if (Array.isArray(entry.link)) {
-                        // NOTE(simon): Try to use the one labeled "alternate" and default to the first one.
-                        const link_node = entry.link.find(link => link["@_rel"] === "alternate") ?? entry.link[0];
-                        if (link_node) {
-                            article.link = link_node["@_href"] ?? link_node["#text"];
+                        // NOTE(simon): Must be defined.
+                        if (item.title) {
+                            article.title = sanitizeHtml(item.title["#text"] ?? "");
                         }
+
+                        // NOTE(simon): The link must exist, but in case it
+                        // doesn't, use the guid instead.
+                        if (item.link) {
+                            article.link = (new URL(item.link["#text"])).href;
+                        } else if (item.guid) {
+                            article.link = (new URL(item.guid["#text"])).href;
+                        }
+
+                        // NOTE(simon): The guid is optional, so use the link if it
+                        // doesn't exist.
+                        if (item.guid) {
+                            article.id = item.guid["#text"];
+                        } else if (item.link) {
+                            article.id = item.link["#text"];
+                        }
+
+                        if (item.pubDate && item.pubDate["#text"]) {
+                            article.published = article.updated = new Date(item.pubDate["#text"]);
+                        }
+
+                        return article;
+                    });
+
+                    if (rss.lastBuildDate) {
+                        feed.updated = new Date(rss.lastBuildDate["#text"]);
+                    } else if (rss.pubDate) {
+                        feed.updated = new Date(rss.pubDate["#text"]);
+                    }
+                } else if (xmlFeed.feed) {
+                    const atom = xmlFeed.feed;
+
+                    if (atom.title) {
+                        feed.title = sanitizeHtml(atom.title["#text"] ?? "");
                     }
 
-                    if (entry.id) {
-                        article.id = entry.id["#text"];
+                    // NOTE(simon): At most one
+                    if (atom.subtitle) {
+                        feed.description = sanitizeHtml(atom.subtitle["#text"] ?? "");
                     }
 
-                    if (entry.published) {
-                        article.published = new Date(entry.published["#text"]);
-                    } else if (entry.published) {
-                        article.published = new Date(entry.updated["#text"]);
+                    // NOTE(simon): Should
+                    if (atom.link) {
+                        feed.link = (new URL(atom.link.find(link => link["@_rel"] === "self")["@_href"] ?? url)).href;
+                    } else {
+                        feed.link = url;
                     }
 
-                    if (entry.published) {
-                        article.updated = new Date(entry.updated["#text"]);
+                    // NOTE(simon): Must
+                    if (atom.id) {
+                        feed.id = atom.id["#text"];
                     }
 
-                    return article;
-                });
+                    feed.articles = atom.entry.map(entry => {
+                        // NOTE(simon): Fill out default values.
+                        const article = {
+                            title:     undefined,
+                            link:      undefined,
+                            id:        undefined,
+                            published: new Date(),
+                            updated:   new Date(),
+                        };
 
-                if (atom.updated && atom.updated["#text"]) {
-                    feed.updated = new Date(atom.updated["#text"]);
+                        if (entry.title) {
+                            article.title = sanitizeHtml(entry.title["#text"]);
+                        }
+
+                        // NOTE(simon): Grab the link. MUST
+                        if (Array.isArray(entry.link)) {
+                            // NOTE(simon): Try to use the one labeled "alternate" and default to the first one.
+                            const link_node = entry.link.find(link => link["@_rel"] === "alternate") ?? entry.link[0];
+                            if (link_node) {
+                                article.link = (new URL(link_node["@_href"] ?? link_node["#text"])).href;
+                            }
+                        }
+
+                        if (entry.id) {
+                            article.id = entry.id["#text"];
+                        }
+
+                        if (entry.published) {
+                            article.published = new Date(entry.published["#text"]);
+                        } else if (entry.published) {
+                            article.published = new Date(entry.updated["#text"]);
+                        }
+
+                        if (entry.published) {
+                            article.updated = new Date(entry.updated["#text"]);
+                        }
+
+                        return article;
+                    });
+
+                    if (atom.updated && atom.updated["#text"]) {
+                        feed.updated = new Date(atom.updated["#text"]);
+                    }
                 }
+            } catch (error) {
+                console.error(`Failed to parse feed of '${url}': ${error.message}`);
             }
 
             return feed;
