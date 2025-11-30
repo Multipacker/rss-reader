e665eb9e91483ca8d3e391cb87d7c5b085d96bf1
Simon Renhult <simon.renhult@hotmail.se>
2025-11-30 22:47:30 +0100

    Refactor: Pull out common code for searching and displaying item lists.

--- static/index.js
+++ static/index.js
@@ -55,7 +55,7 @@ const highlight = (parts) => parts.map((value, index) => {
 const update_list = () => {
     // NOTE(simon): Acquire DOM elements.
     const search_type = document.getElementById("search_type");
-    const search = document.getElementById("search");
+    const search      = document.getElementById("search");
     const result_list = document.getElementById("results");
 
     // NOTE(simon): Construct queries.
@@ -71,119 +71,93 @@ const update_list = () => {
         "i"
     );
 
+    // NOTE(simon): Fetch the item template.
     const template = document.getElementById("template");
+
+    // NOTE(simon): Collect diplay information.
+    let displayItems = [];
     switch (search_type.value) {
         case "Articles": {
-            result_list.replaceChildren(
-                ...entries
-                .map(item => {
-                    let new_item = {...item};
-
-                    const feed = feeds.get(item.feed);
-                    new_item.author = feed.title ?? "";
-
-                    if (search_terms.length !== 0) {
-                        new_item.title  = new_item.title.split(search_regex);
-                        new_item.author = new_item.author.split(search_regex);
-                        new_item.title_matches  = Math.floor((new_item.title.length - 1) / 2);
-                        new_item.author_matches = Math.floor((new_item.author.length - 1) / 2);
-                    } else {
-                        new_item.title  = [new_item.title];
-                        new_item.author = [new_item.author];
-                        new_item.title_matches  = 0;
-                        new_item.author_matches = 0;
-                    }
-
-                    return new_item;
-                })
-                .filter(item => {
-                    return item.title_matches >= search_terms.length || item.author_matches >= search_terms.length;
-                })
-                .sort((a, b) => {
-                    // NOTE(simon): More title matches should appear earlier.
-                    if (a.title_matches !== b.title_matches) {
-                        return b.title_matches - a.title_matches;
-                    }
-
-                    // NOTE(simon): More author matches should appear earlier.
-                    if (a.author_matches !== b.author_matches) {
-                        return b.author_matches - a.author_matches;
-                    }
-
-                    // NOTE(simon): Newer dates should appear earlier.
-                    return b.published - a.published;
-                })
-                .map(item => {
-                    const elem = document.importNode(template.content, true);
-
-                    const htmlItem        = elem.querySelector(".item");
-                    const itemLink        = elem.querySelector(".item-link");
-                    const itemDescription = elem.querySelector(".item-description");
-
-                    if (read_articles.has(item.id)) {
-                        htmlItem.classList.add("read");
-                    }
-                    itemLink.append(...highlight(item.title));
-                    itemLink.setAttribute("href", item.link);
-                    itemLink.onclick = () => save_read(item.id);
-                    itemDescription.append(...highlight(item.author), document.createTextNode(` ${item.published.toLocaleString()}`));
-
-                    return elem;
-                })
-            );
+            displayItems = entries.map(item => {
+                return {
+                    title:       item.title,
+                    description: `${feeds.get(item.feed).title} ${item.published.toLocaleString()}`,
+                    link:        item.link,
+                    date:        item.published,
+                    id:          item.id,
+                    readable:    true,
+                };
+            });
         } break;
         case "Feeds": {
-            result_list.replaceChildren(
-                ...feeds
+            displayItems = feeds
                 .values()
-                .map(feed => {
-                    let new_feed = {...feed};
-
-                    if (search_terms.length !== 0) {
-                        new_feed.title       = new_feed.title.split(search_regex);
-                        new_feed.description = new_feed.description.split(search_regex);
-                        new_feed.title_matches       = Math.floor((new_feed.title.length - 1) / 2);
-                        new_feed.description_matches = Math.floor((new_feed.description.length - 1) / 2);
-                    } else {
-                        new_feed.title       = [new_feed.title];
-                        new_feed.description = [new_feed.description];
-                        new_feed.title_matches       = 0;
-                        new_feed.description_matches = 0;
-                    }
-
-                    return new_feed;
-                })
-                .filter(feed => {
-                    return feed.title_matches >= search_terms.length || feed.description_matches >= search_terms.length;
-                })
                 .toArray()
-                .sort((a, b) => {
-                    // NOTE(simon): More title matches should appear earlier.
-                    if (a.title_matches !== b.title_matches) {
-                        return b.title_matches - a.title_matches;
-                    }
-
-                    // NOTE(simon): More description matches should appear earlier.
-                    if (a.description_matches !== b.description_matches) {
-                        return b.description_matches - a.description_matches;
-                    }
-
-                    // NOTE(simon): Newer dates should appear earlier.
-                    return b.updated - a.updated
-                })
                 .map(feed => {
-                    const elem = document.importNode(template.content, true);
-
-                    const itemLink        = elem.querySelector(".item-link");
-                    const itemDescription = elem.querySelector(".item-description");
-
-                    itemLink.append(...highlight(feed.title));
-                    itemLink.setAttribute("href", feed.link);
-                    itemDescription.append(...highlight(feed.description));
-
-                    return elem;
-                })
-            );
+                    return {
+                        title:       feed.title,
+                        description: feed.description,
+                        link:        feed.link,
+                        date:        feed.published,
+                        id:          feed.id,
+                        readable:    false,
+                    };
+                });
         } break;
     }
+
+    result_list.replaceChildren(
+        ...displayItems
+        .map(item => {
+            if (search_terms.length !== 0) {
+                item.title       = item.title.split(search_regex);
+                item.description = item.description.split(search_regex);
+                item.title_matches       = Math.floor((item.title.length - 1) / 2);
+                item.description_matches = Math.floor((item.description.length - 1) / 2);
+            } else {
+                item.title       = [item.title];
+                item.description = [item.description];
+                item.title_matches       = 0;
+                item.description_matches = 0;
+            }
+            return item;
+        })
+        .filter(item => {
+            return item.title_matches >= search_terms.length || item.description_matches >= search_terms.length;
+        })
+        .sort((a, b) => {
+            // NOTE(simon): More title matches should appear earlier.
+            if (a.title_matches !== b.title_matches) {
+                return b.title_matches - a.title_matches;
+            }
+
+            // NOTE(simon): More description matches should appear earlier.
+            if (a.description_matches !== b.description_matches) {
+                return b.description_matches - a.description_matches;
+            }
+
+            // NOTE(simon): Newer dates should appear earlier.
+            return b.date - a.date;
+        })
+        .map(item => {
+            const elem = document.importNode(template.content, true);
+
+            const htmlItem        = elem.querySelector(".item");
+            const itemLink        = elem.querySelector(".item-link");
+            const itemDescription = elem.querySelector(".item-description");
+
+            if (item.readable) {
+                if (read_articles.has(item.id)) {
+                    htmlItem.classList.add("read");
+                }
+                itemLink.onclick = () => save_read(item.id);
+            }
+
+            itemLink.append(...highlight(item.title));
+            itemLink.setAttribute("href", item.link);
+            itemDescription.append(...highlight(item.description));
+
+            return elem;
+        })
+    )
 };
