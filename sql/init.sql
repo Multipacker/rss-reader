86a3b1cd6550e42d14aa4c88083b1b3c46b5e27c
Simon Renhult <simon.renhult@hotmail.se>
2026-08-28 17:59:22 +0200

    Bugfix(storage): Store raw time data for entries
    They are in UTC anyway so the database should not alter it anyway, but
    it was. Somehow, before this change, it recovered the original timezone
    of entries.

--- sql/init.sql
+++ sql/init.sql
@@ -1,29 +1,29 @@
 CREATE TABLE Feeds (
-    id           UUID        NOT NULL PRIMARY KEY DEFAULT uuidv7(),
-    externalId   TEXT        NOT NULL UNIQUE,
-    title        TEXT        NOT NULL,
-    description  TEXT        NOT NULL,
-    url          TEXT        NOT NULL UNIQUE,
-    updated      TIMESTAMPTZ NOT NULL
+    id           UUID      NOT NULL PRIMARY KEY DEFAULT uuidv7(),
+    externalId   TEXT      NOT NULL UNIQUE,
+    title        TEXT      NOT NULL,
+    description  TEXT      NOT NULL,
+    url          TEXT      NOT NULL UNIQUE,
+    updated      TIMESTAMP NOT NULL
 );
 
 CREATE TABLE Entries (
-    id          UUID        NOT NULL PRIMARY KEY DEFAULT uuidv7(),
-    feed        UUID        NOT NULL REFERENCES Feeds(id),
-    externalId  TEXT        NOT NULL UNIQUE,
-    title       TEXT        NOT NULL,
-    url         TEXT        NOT NULL,
-    published   TIMESTAMPTZ NOT NULL,
-    updated     TIMESTAMPTZ NOT NULL
-);
-
-CREATE TABLE UnfetchedSnapshots (
-    url       TEXT NOT NULL,
-    timestamp TEXT NOT NULL,
-    PRIMARY KEY (url, timestamp)
+    id          UUID      NOT NULL PRIMARY KEY DEFAULT uuidv7(),
+    feed        UUID      NOT NULL REFERENCES Feeds(id),
+    externalId  TEXT      NOT NULL UNIQUE,
+    title       TEXT      NOT NULL,
+    url         TEXT      NOT NULL,
+    published   TIMESTAMP NOT NULL,
+    updated     TIMESTAMP NOT NULL
 );
 
 CREATE TABLE SnapshotTimes (
     url     TEXT        NOT NULL PRIMARY KEY,
     updated TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE UnfetchedSnapshots (
+    url       TEXT NOT NULL REFERENCES SnapshotTimes(url),
+    timestamp TEXT NOT NULL,
+    PRIMARY KEY (url, timestamp)
 );
