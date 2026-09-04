0327666088afb1d7a5c6183fc68f6fc03f7f922f
Simon Renhult <simon.renhult@hotmail.se>
2026-09-04 12:20:45 +0200

    Dev(db): Added a helper for performing transactions and `SendBatch` function to the `Database` interface

--- src/db/db.go
+++ src/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgconn"
@@ -14,6 +15,27 @@ type Database interface {
 	Exec(context context.Context, sql string, arguments ...any) (commandTag pgconn.CommandTag, err error)
 	Query(context context.Context, sql string, args ...any) (pgx.Rows, error)
 	QueryRow(context context.Context, sql string, args ...any) pgx.Row
+	SendBatch(context context.Context, batch *pgx.Batch) pgx.BatchResults
+}
+
+func Transaction(context context.Context, db Database, transaction func(Database) error) error {
+	tx, err := db.Begin(context)
+	if err != nil {
+		return fmt.Errorf("failed to start transaction: %w", err)
+	}
+	defer tx.Rollback(context)
+
+	err = transaction(tx)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit(context)
+	if err != nil {
+		return fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return nil
 }
 
 
